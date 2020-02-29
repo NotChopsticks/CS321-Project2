e05adb30aed33ef9b510846b431ec94704923320
NotChopsticks <Notchopsticks@github.com>
2020-02-29 17:47:00 -0600

    Tournament Implentation

--- src/Menu.java
+++ src/Menu.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many rounds do you want in the sub-tournaments?");
+        while (true) {
+            String input = in.nextLine();
+            String[] token = input.split(" ");
+            int subRounds;
+            try {
+                subRounds = Integer.parseInt(token[0]);
+                System.out.println("Initializing sub-tournaments with " + subRounds + " Rounds");
 
+
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input: Please enter a valid integer");
+            }
+        }
     }
 
-    public void RunTournament(){
-
+    public void RunTournament(int subRounds){
+        Tournament thunderDome = new Tournament();
+        thunderDome.InitializeTournament(subRounds);
     }
 
 }
