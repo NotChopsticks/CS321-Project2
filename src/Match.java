416b128cc3e1805957b712c0f422863c50ae1381
Adam Pittman <adam.w.pittman@gmail.com>
2020-03-04 17:11:33 -0600

    Finished fighter class plus minor match change
    Included get attack and defense performance function calls in Match just so you wouldn't have to type them out.

--- src/Match.java
+++ src/Match.java
@@ -1,2 +1,29 @@
 public class Match {
+
+    Fighter fighter1;
+    Fighter fighter2;
+    Jester jester;
+
+    public Match()
+    {
+       
+    }
+
+    public void PlayMatch()
+    {
+        // These are the function calls that will be used every combat turn to get the
+        // attack and defense performances for each fighter.
+        // Figured I'd include these cause they're long and there's a lot of 
+        // parentheses.
+        fighter1.getAttackPerformance(fighter1.strongerThan(fighter2.strength), fighter1.fasterThan(fighter2.speed));
+        fighter2.getAttackPerformance(fighter2.strongerThan(fighter1.strength), fighter1.fasterThan(fighter1.speed));
+
+        fighter1.getDefensePerformance(fighter1.longerReachThan(fighter2.reach), fighter1.fasterThan(fighter2.speed));
+        fighter2.getDefensePerformance(fighter2.longerReachThan(fighter1.reach), fighter1.fasterThan(fighter1.speed));
+    }
+
+    private void SignalMiddleToJester()
+    {
+
+    }
 }
