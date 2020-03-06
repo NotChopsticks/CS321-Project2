1d699d1508c10365aa2a161ab0dbe621a052dd57
Jonathan Lorray <43081271+NotChopsticks@users.noreply.github.com>
2020-03-06 12:45:01 -0600

    Changes to Menu, Tournament, and made User class to act as the main

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+    public Menu() {
+
+    }
+
+    public void UserInterface() {
         Scanner in = new Scanner(System.in);
-        System.out.println("How many rounds do you want in the sub-tournaments?");
+        System.out.println("How many rounds do you want in the sub-tournaments? /n 1 - 2 Matches /n 2 - 4 Matches /n 3 - 8 Matches /n 4 - 16 Matches /n 5 - 32 Matches /n 6 - 64 Matches");
         while (true) {
             String input = in.nextLine();
             String[] token = input.split(" ");
@@ -11,8 +15,8 @@ public class Menu {
             try {
                 subRounds = Integer.parseInt(token[0]);
                 System.out.println("Initializing sub-tournaments with " + subRounds + " Rounds");
-
-
+                RunTournament(subRounds);
+                break;
             }
             catch (NumberFormatException e) {
                 System.out.println("Invalid input: Please enter a valid integer");
@@ -23,6 +27,7 @@ public class Menu {
     public void RunTournament(int subRounds){
         Tournament thunderDome = new Tournament();
         thunderDome.InitializeTournament(subRounds);
+        thunderDome.DetermineWinner();
     }
 
 }
--- src/Tournament.java
+++ src/Tournament.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Tournament {
 
     List<SubTournament> subTournaments;
+    Random matchMaker = new Random();
+    Fighter winner = null;
 
 
     public Tournament() {
@@ -11,6 +14,30 @@ public class Tournament {
     }
 
     public void DetermineWinner() {
+        for (SubTournament subBracket : subTournaments) {
+            subBracket.DetermineWinner();
+        }
+        int index = matchMaker.nextInt(3);
+        Fighter wildCard = subTournaments.get(3).winner;
+        Fighter SemiFinalist1 = subTournaments.get(index).winner;
+        subTournaments.remove(index);
+        Fighter SemiFinalist2 = subTournaments.get(0).winner;
+        Fighter SemiFinalist3 = subTournaments.get(1).winner;
+
+        Match semiFinal1 = new Match(SemiFinalist2, SemiFinalist3);
+        Match semiFinal2 = new Match(SemiFinalist1, wildCard);
+
+        semiFinal1.PlayMatch();
+        semiFinal2.PlayMatch();
+
+        Match FinalMatch = new Match(semiFinal1.winner, semiFinal2.winner);
+        FinalMatch.PlayMatch();
+        winner = FinalMatch.winner;
+
+//        Fighter longWeaponChampion = subTournaments.get(0).winner;
+//        Fighter mediumWeaponChampion = subTournaments.get(1).winner;
+//        Fighter shortWeaponChampion = subTournaments.get(2).winner;
+//        Fighter wildWeaponChampion = subTournaments.get(3).winner;
 
     }
 
